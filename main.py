aca23307d329c3d5fbf61e9e4f6eddb593eecc69
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-17 07:28:12 +0430

    Add file if not exist.

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import os
 from datetime import date
 
 FILE_NAME = "data.csv"
+
+if not os.path.exists(FILE_NAME):
+    with open(FILE_NAME, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["date", "type", "amount", "note"])
