6fa56d8b320c82317acd04bd89dd48f2859d7b0a
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-17 07:30:44 +0430

    add_transaction function added.

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ if not os.path.exists(FILE_NAME):
     with open(FILE_NAME, "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(["date", "type", "amount", "note"])
+
+
+def add_transaction(tx_type, amount, note):
+    today = date.today().isoformat()
+    with open(FILE_NAME, "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([today, tx_type, amount, note])
+    print("Transaction saved.")
